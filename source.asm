18b4a7d991f184fecb260afa17d1822ce01eb0e9
ibrahim-akrab <ibrahim.m.akrab@gmail.com>
2017-11-19 11:13:48 +0200

    ADD: Move two players using WASD and arrows FIX: Make cursor invisible before drawing anything

--- source.asm
+++ source.asm
@@ -1,24 +1,5 @@
 include maze-drawing-macro.inc
-
-
-
-
-GetIndex MACRO X,Y
-    PUSHA
-  
-    MOV INDEX,00
-    MOV BL,Y
-    MOV AL,81
-    MUL BL
-    ADD INDEX,AX
-    ADD INDEX,X
-    
-        
-    POPA
-        
-ENDM GetIndex
-
-
+include player-movement-macro.inc
 
 
 
@@ -29,13 +10,32 @@ ENDM GetIndex
           
 RANDOM      DB       0  ; THE RANDOM NUBMBER RETURNED BY RANDOMIZE MACRO
 MAZES_N     DB       38 ; NUMBER OF MAZES AVAILABLE FOR EACH MODE
-MODE        DB       '1'; '1'->EASY  ,  '2'->HARD
+MODE        DB       '2'; '1'->EASY  ,  '2'->HARD
 ROWS        DW       81 ; NUMBER OF CHARS IN THE ROW
 COLS        DW       21 ; NUMBER OF CHARS IN THE COL
-INDEX       DB       ?  ; INDEX RETURNED BY GETINDEX MACRO          
+INDEX       DW       ?  ; INDEX RETURNED BY GETINDEX MACRO          
 EASY_MAZE   DB  "MAZE_E.txt",0
 HARD_MAZE   DB  "MAZE_H.txt",0
 MAZE        DB  2100 DUP('$')
+
+X1          DB       0
+Y1          DB       1
+
+X2          DB       0
+Y2          DB       1
+
+
+
+;====== KEYS SCANCODE ============
+UP_ARROW		EQU		48H
+DOWN_ARROW		EQU		50H
+RIGHT_ARROW		EQU		4DH
+LEFT_ARROW		EQU		4BH
+
+W_LETTER		EQU		11H
+A_LETTER		EQU		1EH
+S_LETTER		EQU		1FH
+D_LETTER		EQU		20H      
 
 ; MAXIMUM NUMBER OF MAZES IN EACH MODE IS: 38
 
@@ -51,17 +51,17 @@ MAIN    PROC FAR
         
         DrawMaze
         
-;        
-;       ; MOV AH,02
-;;        MOV BH,00
-;         MOV DL,78 ; BL 3RD   X
-;         MOV DH,00 ; BL TOL   Y
-;;        INT 10H
-;        GetIndex dl,dh
-;        mov ah,2
-;        mov dl,'M'
-;        int 21h
-;           
+
+        MAINLOOP:
+			PrintPlayers
+			HandlePlayerMovement
+			
+			JMP MAINLOOP
+			
+            
+            
+            
+
 
         ; TERMINATE
         MOV AH, 4CH
